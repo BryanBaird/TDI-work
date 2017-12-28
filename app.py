3284052818180f2a89d8fc4ff3641c911091ee09
Bryan Baird <vagrant@s201801-16.localdomain>
2017-12-28 17:41:14 +0000

    First steps

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import pandas as pd
+
+stem = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES'
+filetype_csv = '.csv?'
+t_query = 'ticker='
+ticker = 'GOOG' #returns Google stock prices by default
+
+a_query = 'api_key='
+api_key = 'uMSAPoLS_hMKaPdsa9y2'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    small_csv_query = stem + filetype_csv + t_query + ticker + '&' + a_query + api_key
+    df_full_csv = pd.read_csv(small_csv_query, index_col='date', parse_dates=True)
+    print(df_full_csv.head()
+    return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
