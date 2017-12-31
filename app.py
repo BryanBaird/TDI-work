82a51e8f81fef4636262424516f1a8488f61fa64
Bryan Baird <bryan.eric.baird@gmail.com>
2017-12-31 16:15:16 +0000

    Trying to link a second page from Flask

--- app.py
+++ app.py
@@ -12,12 +12,32 @@ api_key = 'uMSAPoLS_hMKaPdsa9y2'
 
 app = Flask(__name__)
 
-@app.route('/')
+app.vars = {}
+
+@app.route('/', methods=['GET'])
 def index():
     small_csv_query = stem + filetype_csv + t_query + ticker + '&' + a_query + api_key
     df_full_csv = pd.read_csv(small_csv_query, index_col='date', parse_dates=True)
     print(df_full_csv.head())
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index():
+    # request was a POST
+    app.vars['ticker'] = request.form['ticker']
+    app.vars['options'] = request.form['options']
+
+    f = open('%s_%s.txt'%(app.vars['ticker'],app_lulu.vars['options']),'w')
+    f.write('Ticker: %s\n'%(app.vars['ticker']))
+    f.write('Options: %s\n\n'%(app.vars['options']))
+    f.close()
+
+    #return redirect('/main_lulu')
+    return render_template('index2.html')
+
+@app.route('/graph', methods=['POST'])
+def graph():
+    
 
 if __name__ == '__main__':
   app.run(port=33507)
