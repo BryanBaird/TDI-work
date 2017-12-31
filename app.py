4ed3941ed7fbf691951de119800518a59a6d9350
Bryan Baird <bryan.eric.baird@gmail.com>
2017-12-31 20:04:30 +0000

    cleaned up some dataframe references, experimenting with commenting out obsolete code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
+import jinja2
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import components
@@ -30,24 +31,29 @@ def index2():
     app.vars['ticker'] = request.form['ticker']
     app.vars['options'] = request.form['options']
 
-    f = open('%s_%s.txt'%(app.vars['ticker'],app.vars['options']),'w')
-    f.write('Ticker: %s\n'%(app.vars['ticker']))
-    f.write('Options: %s\n\n'%(app.vars['options']))
-    f.close()
+    #f = open('%s_%s.txt'%(app.vars['ticker'],app.vars['options']),'w')
+    #f.write('Ticker: %s\n'%(app.vars['ticker']))
+    #f.write('Options: %s\n\n'%(app.vars['options']))
+    #f.close()
 
     return redirect('/graph')
 
 @app.route('/graph', methods=['GET', 'POST'])
 def graph():
-    small_csv_query = stem + filetype_csv + t_query + app.vars['ticker'] + '&' + a_query + api_key
-    df_full_csv = pd.read_csv(small_csv_query, index_col='date', parse_dates=True)
+    csv_query = stem + filetype_csv + t_query + app.vars['ticker'] + '&' + a_query + api_key
+    print('API call: ' + csv_query)
+    df = pd.read_csv(csv_query, index_col='date', parse_dates=True)
     
+    print(df.head())
     plot = figure(tools="pan,wheel_zoom,box_zoom,reset", title='%s Opening Stock Price' %(app.vars['ticker']), x_axis_label='date', x_axis_type='datetime')
 
     # add a line renderer
-    plot.line(small_csv_query.index, df_full_csv['open'], line_width=2)
+    plot.line(df.index, df['open'], line_width=2)
     
     script, div = components(plot)
+    print('Script: ' + script)
+    print('div :' + div)
+    
     return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
